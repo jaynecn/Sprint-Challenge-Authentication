e40adb6d06f76a8cebcd9c6c5c92b14ad1474229
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-11-15 09:24:19 +0000

    server working and basic data going to the server

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// middleware & connecting
+const Users = require('../auth/auth-model');
+const secrets = require('../config/secrets');
+const authenticate = require('./authenticate-middleware');
+
+// GET request - for testing connection
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello from the api'})
+})
+
+// POST requests
 
 router.post('/register', (req, res) => {
   // implement registration
